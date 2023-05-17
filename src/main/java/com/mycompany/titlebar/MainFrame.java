23fd71fe0800b9b982bac3d4e455c2dcb6a257f3
Aaron Maraya <aaronmaraya7@gmail.com>
2023-05-17 22:27:51 +0800

    First commit

---
+++ src/main/java/com/mycompany/titlebar/MainFrame.java
@@ -0,0 +1,127 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
+ */
+package com.mycompany.titlebar;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Admin
+ */
+public class MainFrame extends javax.swing.JFrame {
+
+    private final TitleBar TITLEBAR = new TitleBar(this);
+    private int currentState;
+    
+    public MainFrame() {
+        initComponents();
+        goDisplay();
+        System.out.println(getSize());
+        TITLEBAR.init(MainFrame.this);
+    }
+    public void goDisplay(){
+        displayTitleBar2.display(TITLEBAR);
+    }
+    public int restoreState() {
+        currentState = TITLEBAR.getCurrentState();
+        return currentState;
+    }
+    public MainFrame getMain(){
+        return this;
+    }
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        displayTitleBar2 = new com.mycompany.titlebar.DisplayTitleBar();
+        clock1 = new main.Clock();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(574, 351));
+        setUndecorated(true);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowDeiconified(java.awt.event.WindowEvent evt) {
+                formWindowDeiconified(evt);
+            }
+        });
+
+        javax.swing.GroupLayout displayTitleBar2Layout = new javax.swing.GroupLayout(displayTitleBar2);
+        displayTitleBar2.setLayout(displayTitleBar2Layout);
+        displayTitleBar2Layout.setHorizontalGroup(
+            displayTitleBar2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 0, Short.MAX_VALUE)
+        );
+        displayTitleBar2Layout.setVerticalGroup(
+            displayTitleBar2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 31, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(clock1, javax.swing.GroupLayout.DEFAULT_SIZE, 503, Short.MAX_VALUE)
+            .addComponent(displayTitleBar2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(displayTitleBar2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, 0)
+                .addComponent(clock1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void formWindowDeiconified(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowDeiconified
+        if (restoreState() == JFrame.MAXIMIZED_BOTH) {
+            setExtendedState(JFrame.MAXIMIZED_BOTH);
+        } else {
+            setExtendedState(JFrame.NORMAL);
+        }
+    }//GEN-LAST:event_formWindowDeiconified
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(MainFrame.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MainFrame().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private main.Clock clock1;
+    private com.mycompany.titlebar.DisplayTitleBar displayTitleBar2;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/main/java/com/mycompany/titlebar/TitleBar.java
@@ -0,0 +1,181 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JPanel.java to edit this template
+ */
+package com.mycompany.titlebar;
+
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Toolkit;
+import java.awt.event.ComponentListener;
+import javax.swing.JFrame;
+
+public class TitleBar extends javax.swing.JPanel {
+    
+    private int currentState;
+    private Point pressed;
+    private final MainFrame MAIN;
+    private ComponentResizer resize;
+
+    public TitleBar(MainFrame MAIN) {
+        this.MAIN = MAIN;
+        initComponents();
+    }
+    public void init(JFrame MAIN) {
+        resize = new ComponentResizer();
+        resize.setSnapSize(new Dimension(10, 10));
+        resize.setMinimumSize(new Dimension(300, 200));
+        resize.setMaximumSize(Toolkit.getDefaultToolkit().getScreenSize());
+        resize.registerComponent(MAIN);
+    }
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        moveableButton = new javax.swing.JButton();
+        exitButton = new javax.swing.JButton();
+        minimizeButton = new javax.swing.JButton();
+        maximizeButton = new javax.swing.JButton();
+
+        setBackground(new java.awt.Color(39, 39, 39));
+
+        moveableButton.setContentAreaFilled(false);
+        moveableButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        moveableButton.addMouseMotionListener(new java.awt.event.MouseMotionAdapter() {
+            public void mouseDragged(java.awt.event.MouseEvent evt) {
+                moveableButtonMouseDragged(evt);
+            }
+        });
+        moveableButton.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                moveableButtonMouseClicked(evt);
+            }
+            public void mousePressed(java.awt.event.MouseEvent evt) {
+                moveableButtonMousePressed(evt);
+            }
+        });
+        moveableButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                moveableButtonActionPerformed(evt);
+            }
+        });
+
+        exitButton.setIcon(new javax.swing.ImageIcon("C:\\Users\\Admin\\Downloads\\close.png")); // NOI18N
+        exitButton.setContentAreaFilled(false);
+        exitButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        exitButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                exitButtonActionPerformed(evt);
+            }
+        });
+
+        minimizeButton.setIcon(new javax.swing.ImageIcon("C:\\Users\\Admin\\Downloads\\minus.png")); // NOI18N
+        minimizeButton.setContentAreaFilled(false);
+        minimizeButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        minimizeButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                minimizeButtonActionPerformed(evt);
+            }
+        });
+
+        maximizeButton.setIcon(new javax.swing.ImageIcon("C:\\Users\\Admin\\Downloads\\copy.png")); // NOI18N
+        maximizeButton.setContentAreaFilled(false);
+        maximizeButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
+        maximizeButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                maximizeButtonActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(moveableButton, javax.swing.GroupLayout.DEFAULT_SIZE, 459, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(minimizeButton, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(maximizeButton, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(exitButton, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(moveableButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                    .addComponent(exitButton, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 32, Short.MAX_VALUE)
+                    .addComponent(minimizeButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(maximizeButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(0, 0, Short.MAX_VALUE))
+        );
+    }// </editor-fold>//GEN-END:initComponents
+    
+    private void exitButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_exitButtonActionPerformed
+        MAIN.dispose();
+    }//GEN-LAST:event_exitButtonActionPerformed
+
+    private void minimizeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_minimizeButtonActionPerformed
+        MAIN.setExtendedState(JFrame.ICONIFIED);
+    }//GEN-LAST:event_minimizeButtonActionPerformed
+
+    private void maximizeButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_maximizeButtonActionPerformed
+        if(MAIN.isResizable()){
+            if (MAIN.getExtendedState() == JFrame.MAXIMIZED_BOTH) {
+                MAIN.setExtendedState(JFrame.NORMAL);
+                currentState = 1;
+            } else {
+                MAIN.setExtendedState(JFrame.MAXIMIZED_BOTH);
+                currentState = 6;
+            }
+        }
+    }//GEN-LAST:event_maximizeButtonActionPerformed
+
+    private void moveableButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_moveableButtonActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_moveableButtonActionPerformed
+
+    private void moveableButtonMousePressed(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_moveableButtonMousePressed
+        pressed = evt.getPoint();
+    }//GEN-LAST:event_moveableButtonMousePressed
+
+    private void moveableButtonMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_moveableButtonMouseClicked
+        if (evt.getClickCount() == 2) {
+            if (MAIN.getExtendedState() == JFrame.MAXIMIZED_BOTH) {
+                MAIN.setExtendedState(JFrame.NORMAL);
+                currentState = 1;
+            } else {
+                MAIN.setExtendedState(JFrame.MAXIMIZED_BOTH);
+                currentState = 6;
+            }
+        }
+    }//GEN-LAST:event_moveableButtonMouseClicked
+
+    private void moveableButtonMouseDragged(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_moveableButtonMouseDragged
+        MAIN.setExtendedState(JFrame.NORMAL);
+        int thisX = MAIN.getLocation().x;
+        int thisY = MAIN.getLocation().y;
+
+        int xMoved = evt.getX() - pressed.x;
+        int yMoved = evt.getY() - pressed.y;
+
+        int X = thisX + xMoved;
+        int Y = thisY + yMoved;
+        MAIN.setLocation(X, Y);
+    }//GEN-LAST:event_moveableButtonMouseDragged
+    
+    public int getCurrentState(){
+        return currentState;
+    }
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton exitButton;
+    private javax.swing.JButton maximizeButton;
+    private javax.swing.JButton minimizeButton;
+    private javax.swing.JButton moveableButton;
+    // End of variables declaration//GEN-END:variables
+}
